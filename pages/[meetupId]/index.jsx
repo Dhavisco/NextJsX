1b8ec82744b281cf62a888767b9f2538d52cc88f
Dhavisco <olaniyidavid2002@gmail.com>
2024-06-03 14:43:37 +0100

    meetup detail

---
+++ pages/[meetupId]/index.jsx
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+export default function MeetupDetails(){
+    return (
+      <>
+        <MeetupDetail
+          image="https://th.bing.com/th/id/R.171469519d57e63bf0cb18c3257797e3?rik=Rs1FirAHPKiyXA&riu=http%3a%2f%2fwww.baltana.com%2ffiles%2fwallpapers-2%2fFruit-HD-Wallpapers-03484.jpg&ehk=7L0lkg9TPcRVu%2bjsZhWvhNw1CZRgILylaYrEFKMa6n4%3d&risl=&pid=ImgRaw&r=0"
+          title="A first Meetup"
+          address="Some Street 5, Some City"
+          description="The meetup description"
+        />
+        </>
+    );
+}
