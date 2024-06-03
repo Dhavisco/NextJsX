f11ac831b203b5c3ea65891189534709da8fff1b
Dhavisco <olaniyidavid2002@gmail.com>
2024-06-03 13:45:59 +0100

    added main navigation

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,11 @@
 import "../app/globals.css"; // Adjust the path as necessary
 import RootLayout from "../app/layout"; // Adjust the path as necessary
-
+import Layout from '../components/layout/Layout'
 function MyApp({ Component, pageProps }) {
   return (
+    <Layout>
       <Component {...pageProps} />
+    </Layout>
   );
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,23 @@
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A first Meetup',
-        image: 'file:///C:/Users/user/Downloads/fruit1.jpg',
-        address: 'Some address 5 Toyin',
-        description: 'This is the first meetup'
-    },
-    {
-        id: 'm2',
-        title: 'A second Meetup',
-        image: 'file:///C:/Users/user/Downloads/fruit1.jpg',
-        address: 'Some address 5 Toyin',
-        description: 'This is the second meetup'
-    }
-]
+  {
+    id: "m1",
+    title: "A first Meetup",
+    image:
+      "https://th.bing.com/th/id/R.171469519d57e63bf0cb18c3257797e3?rik=Rs1FirAHPKiyXA&riu=http%3a%2f%2fwww.baltana.com%2ffiles%2fwallpapers-2%2fFruit-HD-Wallpapers-03484.jpg&ehk=7L0lkg9TPcRVu%2bjsZhWvhNw1CZRgILylaYrEFKMa6n4%3d&risl=&pid=ImgRaw&r=0",
+    address: "Some address 5 Toyin",
+    description: "This is the first meetup",
+  },
+  {
+    id: "m2",
+    title: "A second Meetup",
+    image:
+      "https://th.bing.com/th/id/R.171469519d57e63bf0cb18c3257797e3?rik=Rs1FirAHPKiyXA&riu=http%3a%2f%2fwww.baltana.com%2ffiles%2fwallpapers-2%2fFruit-HD-Wallpapers-03484.jpg&ehk=7L0lkg9TPcRVu%2bjsZhWvhNw1CZRgILylaYrEFKMa6n4%3d&risl=&pid=ImgRaw&r=0",
+    address: "Some address 5 Toyin",
+    description: "This is the second meetup",
+  },
+];
 
 function HomePage() {
     return <MeetupList meetups={DUMMY_MEETUPS} />
