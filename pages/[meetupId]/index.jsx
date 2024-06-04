2323ef094ef10759601c4a6c275cae4465d444a0
Dhavisco <olaniyidavid2002@gmail.com>
2024-06-04 19:57:31 +0100

    getstaticpath

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,6 +1,7 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail"
 
 export default function MeetupDetails(){
+
     return (
       <>
         <MeetupDetail
@@ -11,4 +12,42 @@ export default function MeetupDetails(){
         />
         </>
     );
+}
+
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context){
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  //fetch data for a single meetup
+  return{
+    props: {
+      meetupData: {
+         image: "https://th.bing.com/th/id/R.171469519d57e63bf0cb18c3257797e3?rik=Rs1FirAHPKiyXA&riu=http%3a%2f%2fwww.baltana.com%2ffiles%2fwallpapers-2%2fFruit-HD-Wallpapers-03484.jpg&ehk=7L0lkg9TPcRVu%2bjsZhWvhNw1CZRgILylaYrEFKMa6n4%3d&risl=&pid=ImgRaw&r=0",
+          id: meetupId,
+         title: "A first Meetup",
+          address: "Some Street 5, Some City",
+          description:"The meetup description",
+      }
+    }
+  }
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -26,28 +26,28 @@ function HomePage(props) {
 }
 
 
-export async function getServerSideProps(context){
-  const req = context.req;
-  const res = context.res;
-  
-  // fetch data from an API
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
+// export async function getServerSideProps(context){
+//   const req = context.req;
+//   const res = context.res;
 
-}
-
-// export async function getStaticProps() {
-//   //fetch data from an API
-//   return{
+//   // fetch data from an API
+//   return {
 //     props: {
-//       meetups: DUMMY_MEETUPS
+//       meetups: DUMMY_MEETUPS,
 //     },
-//     revalidate: 10
 //   };
+
 // }
+
+export async function getStaticProps() {
+  //fetch data from an API
+  return{
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 10
+  };
+}
 
 export default HomePage;
 
