44185f332aeeb9c6f859afb08dbd82114a319458
Dhavisco <olaniyidavid2002@gmail.com>
2024-06-05 21:33:57 +0100

    updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ const DUMMY_MEETUPS = [
     title: "A first Meetup",
     image:
       "https://th.bing.com/th/id/R.171469519d57e63bf0cb18c3257797e3?rik=Rs1FirAHPKiyXA&riu=http%3a%2f%2fwww.baltana.com%2ffiles%2fwallpapers-2%2fFruit-HD-Wallpapers-03484.jpg&ehk=7L0lkg9TPcRVu%2bjsZhWvhNw1CZRgILylaYrEFKMa6n4%3d&risl=&pid=ImgRaw&r=0",
-    address: "Some address 5 Toyin",
+    address: "Some address 5 Toyin Street",
     description: "This is the first meetup",
   },
   { 
@@ -14,7 +14,7 @@ const DUMMY_MEETUPS = [
     title: "A second Meetup",
     image:
       "https://th.bing.com/th/id/R.171469519d57e63bf0cb18c3257797e3?rik=Rs1FirAHPKiyXA&riu=http%3a%2f%2fwww.baltana.com%2ffiles%2fwallpapers-2%2fFruit-HD-Wallpapers-03484.jpg&ehk=7L0lkg9TPcRVu%2bjsZhWvhNw1CZRgILylaYrEFKMa6n4%3d&risl=&pid=ImgRaw&r=0",
-    address: "Some address 5 Toyin",
+    address: "Some address 5 Toyin Street",
     description: "This is the second meetup",
   },
 ];
