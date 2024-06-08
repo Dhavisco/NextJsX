677ffa1a80779366aeb3b95957c274092c423f52
Dhavisco <olaniyidavid2002@gmail.com>
2024-06-08 21:20:47 +0100

    connection to mongo db

--- pages/api/new-meetup.jsx
+++ pages/api/new-meetup.jsx
@@ -1,11 +1,35 @@
+import {MongoClient} from 'mongodb'
+
 // /api/new-meetup
+// POST /api/new-meetup
 
-function handler(req, res){
-    if (req.method === 'POST') {
-        const data = req.body;
+async function handler(req, res){
+    if (req.method === "POST") {
+    try {
+      const data = req.body;
 
-        const {title, image, address, description} = data;
-    } 
+      const client = await MongoClient.connect(
+        "mongodb+srv://olaniyidavid2002:davisco32@cluster0.ype1yze.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+      );
+      const db = client.db();
+
+      const meetupsCollection = db.collection("meetups");
+
+      const result = await meetupsCollection.insertOne({ data });
+
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({ message: "Meetup Inserted!" });
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+    } else {
+      // Handle any other HTTP method
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
 }
 
 export default handler;
--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -1,8 +1,24 @@
 //our-domain.com/new-meetup
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 function NewMeetupPage() {
-    function addMeetupHandler(enteredMeetupData){
-       console.log(enteredMeetupData)
+    async function addMeetupHandler(enteredMeetupData){
+      const response = await fetch('/api/new-meetup', {
+         method: 'POST',
+         body:JSON.stringify(enteredMeetupData),
+         headers: {
+            'Content-Type': 'application/json'
+         }
+      });
+
+      if(!response.ok){
+         throw new Error('Failed to create meetup')
+      }
+
+      const data = await response.json();
+
+      console.log(enteredMeetupData);
+
+      console.log(data);
     }
      return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
 }
